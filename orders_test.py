c8ba571757ce009693de88c287d7fea71c4e3b66
Hardash-GIT <hardash@bk.ru>
2023-11-17 20:53:51 +0300

    diploma

--- orders_test.py
+++ orders_test.py
@@ -1,6 +1,7 @@
 import sender_stand_request
 import data
 
+# Евгений Бондарь, 10-я когорта — Финальный проект. Инженер по тестированию плюс
 def test_create_order_positive():
     response = sender_stand_request.post_new_order(data.order_body)
     assert response.status_code == 201
